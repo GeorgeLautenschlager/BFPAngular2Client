2c595f0a0453b4a688ff39a6b81a778151e4dfed
George Lautenschlager <george@coupgon.com>
2016-11-26 17:22:53 -0500

    Clean up ship drawing

--- app/gameboard.component.ts
+++ app/gameboard.component.ts
@@ -13,6 +13,13 @@ export class GameboardComponent{
   @Input() ships: Ship[];
   @ViewChild("table") tableTop: ElementRef;
 
+  // The radius of a small base on a 4" by 4" table
+  smallBaseScaleFactor = 0.01639;
+  // Length of an imperial cruiser on a 4" by 4" table
+  shipScaleFactor = 0.01639*4;
+  // 4" by 4" table height in millimetres
+  tableStandardHeight = 1219.2;
+
   selectedShip: Ship;
 
   constructor() { }
@@ -26,16 +33,20 @@ export class GameboardComponent{
   }
 
   ngAfterViewInit() {
-    const shipLength = this.tableTop.nativeElement.width * 0.01639;
+    this.drawShips();
+  }
+
+  drawShips() {
     let tableWidth = this.tableTop.nativeElement.width;
     let tableHeight = this.tableTop.nativeElement.height;
+    const shipLength = tableHeight * this.shipScaleFactor;
 
     let ctx: CanvasRenderingContext2D = this.tableTop.nativeElement.getContext("2d");
 
     for (let ship of this.ships) {
       // scale the ships coordinates
-      const x = ship.locationX/2438.4 * tableWidth;
-      const y = ship.locationY/1219.2 * tableHeight;
+      const x = ship.locationX/(this.tableStandardHeight*2)*tableWidth;
+      const y = ship.locationY/(this.tableStandardHeight)*tableHeight;
 
       // Imperial Green, or what passes for it
       ctx.fillStyle = '#265401';
@@ -43,10 +54,9 @@ export class GameboardComponent{
       // move forward half a ship length, at same Y
       ctx.moveTo(x + shipLength/2, y);
       // draw line to top right "corner" of rectangle
-      ctx.lineTo(x - shipLength/2, y - shipLength/4)
+      ctx.lineTo(x - shipLength/2, y-shipLength/4)
       // Now draw a line to the bottome left "corner"
-      ctx.lineTo(x - shipLength/2,
-        y + shipLength/4)
+      ctx.lineTo(x - shipLength/2, y+shipLength/4)
       // Now fill the triangle
       ctx.fill();
 
